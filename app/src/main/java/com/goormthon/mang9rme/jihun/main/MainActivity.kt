73783bbc4a43078bb4377c52f7d23a81b657f88d
ChunBaee <mljung10@naver.com>
2023-03-30 11:47:57 +0900

    feat: 메인 액티비티 초기 구성

---
+++ app/src/main/java/com/goormthon/mang9rme/jihun/main/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.goormthon.mang9rme.jihun.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.goormthon.mang9rme.R
+import com.goormthon.mang9rme.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(){
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+    }
+
+
+}
