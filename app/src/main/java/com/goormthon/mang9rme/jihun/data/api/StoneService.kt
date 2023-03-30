e348130fcf67afdc61b87bc69fb5b3c6a60ebaca
ChunBaee <mljung10@naver.com>
2023-03-31 06:34:54 +0900

    feat: 풀 받기용 커밋

--- app/src/main/java/com/goormthon/mang9rme/jihun/data/api/StoneService.kt
+++ app/src/main/java/com/goormthon/mang9rme/jihun/data/api/StoneService.kt
@@ -1,10 +1,14 @@
 package com.goormthon.mang9rme.jihun.data.api
 
+import com.goormthon.mang9rme.jihun.data.BaseResponse
 import com.goormthon.mang9rme.jihun.data.data.StoneFeedResponse
+import com.goormthon.mang9rme.jihun.data.data.StoneNameModifyRequest
 import com.goormthon.mang9rme.jihun.data.data.StoneResponse
 import retrofit2.Call
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.Path
 
 interface StoneService {
@@ -16,4 +20,9 @@ interface StoneService {
     suspend fun getStoneData(
         @Path("stoneId", encoded = true) stoneId : Int
     ) : Response<StoneResponse>
+
+    @PATCH("/api/stone")
+    suspend fun patchStoneName(
+        @Body body : StoneNameModifyRequest
+    ) : Response<BaseResponse>
 }
--- app/src/main/java/com/goormthon/mang9rme/jihun/data/repo/StoneRepository.kt
+++ app/src/main/java/com/goormthon/mang9rme/jihun/data/repo/StoneRepository.kt
@@ -1,10 +1,14 @@
 package com.goormthon.mang9rme.jihun.data.repo
 
 import com.goormthon.mang9rme.common.data.StoneData
+import com.goormthon.mang9rme.jihun.data.BaseResponse
+import com.goormthon.mang9rme.jihun.data.data.StoneNameModifyRequest
 
 interface StoneRepository {
 
     suspend fun getStoneFeedData() : Result<List<StoneData>>
 
     suspend fun getStoneData(stoneId : Int) : Result<StoneData>
+
+    suspend fun patchStoneName(body : StoneNameModifyRequest) : Result<BaseResponse>
 }
