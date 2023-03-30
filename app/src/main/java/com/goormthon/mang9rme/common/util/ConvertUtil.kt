44e79eec7a171f493cda42680f6537784e41bd4a
ByeongSu Kim <kimbsu3000@gmail.com>
2023-03-30 10:30:46 +0900

    feat: Util 클래스 정의

---
+++ app/src/main/java/com/goormthon/mang9rme/common/util/ConvertUtil.kt
@@ -0,0 +1,29 @@
+package com.goormthon.mang9rme.common.util
+
+import android.content.Context
+import android.util.TypedValue
+
+object ConvertUtil {
+
+    /**
+     * @return dp를 px 단위로 변경한 값, Int & 내림처리
+     */
+    fun dpToPx(context: Context, dp: Int): Int = dpToPx(context, dp.toFloat()).toInt()
+
+    /**
+     * @return dp를 px 단위로 변경한 값, Float
+     */
+    fun dpToPx(context: Context, dp: Float): Float =
+        TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.resources.displayMetrics)
+
+    /**
+     * @return px을 dp 단위로 변경한 값, Int & 내림처리
+     */
+    fun pxToDp(context: Context, px: Int): Int = pxToDp(context, px.toFloat()).toInt()
+
+    /**
+     * @return px을 dp 단위로 변경한 값, Float
+     */
+    fun pxToDp(context: Context, px: Float): Float =
+        TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, px, context.resources.displayMetrics)
+}
---
+++ app/src/main/java/com/goormthon/mang9rme/common/util/DLog.kt
@@ -0,0 +1,62 @@
+package com.goormthon.mang9rme.common.util
+
+
+import android.util.Log
+import com.goormthon.mang9rme.BuildConfig
+
+/**
+ * Debug 모드로 빌드된 앱에서만 로그가 찍히도록 하기 위한 오브젝트
+ *
+ * @see Log
+ */
+object DLog {
+
+    fun d(pTag: String, pMessage: String?) {
+        if (BuildConfig.DEBUG && pMessage != null && pMessage.isNotEmpty()) {
+            Log.d(pTag, pMessage)
+        }
+    }
+
+    fun d(pTag: String, pMessage: String?, pException: Exception) {
+        if (BuildConfig.DEBUG && pMessage != null && pMessage.isNotEmpty()) {
+            Log.d(pTag, pMessage, pException)
+        }
+    }
+
+    fun w(pTag: String, pMessage: String?) {
+        if (BuildConfig.DEBUG && pMessage != null && pMessage.isNotEmpty()) {
+            Log.w(pTag, pMessage)
+        }
+    }
+
+    fun w(pTag: String, pMessage: String?, pException: Exception) {
+        if (BuildConfig.DEBUG && pMessage != null && pMessage.isNotEmpty()) {
+            Log.w(pTag, pMessage, pException)
+        }
+    }
+
+    fun e(pTag: String, pMessage: String?) {
+        if (BuildConfig.DEBUG && pMessage != null && pMessage.isNotEmpty()) {
+            Log.e(pTag, pMessage)
+        }
+    }
+
+    fun e(pTag: String, pMessage: String?, pException: Exception) {
+        if (BuildConfig.DEBUG && pMessage != null && pMessage.isNotEmpty()) {
+            Log.e(pTag, pMessage, pException)
+        }
+    }
+
+    fun i(pTag: String, pMessage: String?) {
+        if (BuildConfig.DEBUG && pMessage != null && pMessage.isNotEmpty()) {
+            Log.i(pTag, pMessage)
+        }
+    }
+
+    fun i(pTag: String, pMessage: String?, pException: Exception) {
+        if (BuildConfig.DEBUG && pMessage != null && pMessage.isNotEmpty()) {
+            Log.i(pTag, pMessage, pException)
+        }
+    }
+
+}
