fd9e08dfac7a1fece4dd87b10e67dde3690789b1
ByeongSu Kim <kimbsu3000@gmail.com>
2023-03-30 10:30:28 +0900

    feat: Custom Dialog 정의
    메시지 출력을 위한 MessageDialog, 로딩바 출력을 위한 LoadingProgressDialog를 추가함

---
+++ app/src/main/java/com/goormthon/mang9rme/common/customview/LoadingProgressDialog.kt
@@ -0,0 +1,133 @@
+package com.goormthon.mang9rme.common.customview
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.goormthon.mang9rme.R
+import com.goormthon.mang9rme.databinding.DialogLoadingProgressBinding
+
+class LoadingProgressDialog(
+    context: Context,
+    private val message: String = ""
+) : Dialog(context, R.style.progressBarTheme) {
+
+    private val binding: DialogLoadingProgressBinding by lazy {
+        DialogLoadingProgressBinding.inflate(layoutInflater)
+    }
+
+    private var _animFlag: Boolean = false
+    private val animFlag: Boolean get() = _animFlag
+
+    private val scaleDownAnim: Animation =
+        AnimationUtils.loadAnimation(context, R.anim.loading_progress_scale_down).apply {
+            setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationStart(animatino: Animation?) {
+
+                }
+
+                override fun onAnimationEnd(animatino: Animation?) {
+                    binding.ivLoadingProgress.startAnimation(scaleUpAnim)
+                }
+
+                override fun onAnimationRepeat(animatino: Animation?) {
+
+                }
+            })
+        }
+    private val scaleUpAnim: Animation =
+        AnimationUtils.loadAnimation(context, R.anim.loading_progress_scale_up).apply {
+            setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationStart(animatino: Animation?) {
+
+                }
+
+                override fun onAnimationEnd(animatino: Animation?) {
+                    if (animFlag)
+                        startAnimation()
+                }
+
+                override fun onAnimationRepeat(animatino: Animation?) {
+
+                }
+            })
+        }
+
+    private val mHandler: Handler = object : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            when (msg.what) {
+                // Dialog Message 변경
+                HANDLER_WHAT_SET_MESSAGE -> {
+                    binding.apply {
+                        tvLoadingProgress.text = message
+                        tvLoadingProgress.visibility =
+                            if (message.isEmpty()) View.GONE else View.VISIBLE
+                    }
+                }
+                // Dialog Animation
+                HANDLER_WHAT_START_ANIM -> {
+                    if (animFlag)
+                        binding.ivLoadingProgress.startAnimation(scaleDownAnim)
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window?.attributes = WindowManager.LayoutParams().apply {
+            flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND
+            dimAmount = 0.6f
+        }
+
+        setContentView(binding.root)
+        init()
+    }
+
+    override fun show() {
+        super.show()
+
+        _animFlag = true
+        startAnimation()
+    }
+
+    override fun dismiss() {
+        super.dismiss()
+        _animFlag = false
+    }
+
+    private fun init() {
+        setMessage(message)
+    }
+
+    private fun startAnimation() {
+        val msg = Message.obtain().apply {
+            what = HANDLER_WHAT_START_ANIM
+        }
+        mHandler.sendMessage(msg)
+    }
+
+    fun setMessage(pMessage: String) {
+        val msg = Message.obtain().apply {
+            what = HANDLER_WHAT_SET_MESSAGE
+            obj = pMessage
+        }
+        mHandler.sendMessage(msg)
+    }
+
+    companion object {
+        private const val TAG: String = "LoadingProgressDialog"
+
+        private const val HANDLER_WHAT_SET_MESSAGE: Int = 0
+        private const val HANDLER_WHAT_START_ANIM: Int = 1
+    }
+}
---
+++ app/src/main/java/com/goormthon/mang9rme/common/customview/MessageDialog.kt
@@ -0,0 +1,213 @@
+package com.goormthon.mang9rme.common.customview
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import androidx.fragment.app.DialogFragment
+import com.goormthon.mang9rme.R
+import com.goormthon.mang9rme.common.util.ConvertUtil
+import com.goormthon.mang9rme.databinding.DialogMessageBinding
+
+class MessageDialog : DialogFragment {
+
+    private var _binding: DialogMessageBinding? = null
+    private val binding: DialogMessageBinding get() = _binding!!
+
+    private val message: String
+
+    private var dialogType: DialogType = DialogType.DEFAULT
+
+    private var dismissCallback: (() -> Unit)? = null
+
+    private var btnText: String? = null
+    private var btnClickListener: () -> Unit = { dismiss() }
+
+    private var leftBtnText: String = ""
+    private var leftBtnClickListener: () -> Unit = { dismiss() }
+
+    private var rightBtnText: String = ""
+    private var rightBtnClickListener: () -> Unit = { dismiss() }
+
+    // region Public Constructor
+    /**
+     * 메시지 + 디폴트 버튼
+     *
+     * @param pMsg 다이얼로그 메시지
+     */
+    constructor(pMsg: String) {
+        message = pMsg
+        dialogType = DialogType.DEFAULT
+    }
+
+    /**
+     * 메시지 + 커스텀 버튼 1개
+     *
+     * @param pMsg              다이얼로그 메시지
+     * @param pDismissCallback  다이얼로그 dismiss callback
+     */
+    constructor(pMsg: String, pDismissCallback: () -> Unit) {
+        message = pMsg
+        dismissCallback = pDismissCallback
+        dialogType = DialogType.ONLY_MSG
+    }
+
+    /**
+     * 메시지 + 커스텀 버튼 1개
+     *
+     * @param pMsg              메시지
+     * @param pBtnText          커스텀 버튼 텍스트
+     * @param pBtnClickListener 커스텀 버튼 클릭 이벤트
+     */
+    constructor(
+        pMsg: String,
+        pBtnText: String,
+        pBtnClickListener: () -> Unit
+    ) {
+        message = pMsg
+        btnText = pBtnText
+        btnClickListener = pBtnClickListener
+        dialogType = DialogType.ONE_BTN
+    }
+
+    /**
+     * 메시지 + 커스텀 버튼 2개
+     *
+     * @param pMsg                      메시지
+     * @param pLeftBtnText              확인 버튼 텍스트
+     * @param pLeftBtnClickListener     확인 버튼 클릭 이벤트
+     * @param pRightBtnText             취소 버튼 텍스트
+     * @param pRightBtnClickListener    취소 버튼 클릭 이벤트
+     */
+    constructor(
+        pMsg: String,
+        pLeftBtnText: String,
+        pLeftBtnClickListener: () -> Unit,
+        pRightBtnText: String,
+        pRightBtnClickListener: () -> Unit
+    ) {
+        message = pMsg
+        leftBtnText = pLeftBtnText
+        leftBtnClickListener = pLeftBtnClickListener
+        rightBtnText = pRightBtnText
+        rightBtnClickListener = pRightBtnClickListener
+        dialogType = DialogType.TWO_BTN
+    }
+    // endregion Public Constructor
+
+    // region Fragment LifeCycle
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NO_TITLE, R.style.Mang9rmeDialog)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DialogMessageBinding.inflate(inflater, container, false)
+
+        dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog?.setCanceledOnTouchOutside(false)
+        dialog?.setCancelable(false)
+
+        init()
+
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val params = dialog?.window?.attributes
+        params?.width = ConvertUtil.dpToPx(requireContext(), 334)
+        dialog?.window?.attributes = params
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun dismiss() {
+        super.dismiss()
+
+        dismissCallback?.let { callback ->
+            callback()
+        }
+    }
+    // endregion Fragment LifeCycle
+
+    private fun init() {
+        binding.apply {
+            when (dialogType) {
+                DialogType.DEFAULT,
+                DialogType.ONLY_MSG -> {
+                    binding.apply {
+                        tvMessageDialogMsg.text = message
+                        tvMessageDialogLeftBtn.text = "확인"
+                        tvMessageDialogLeftBtn.setOnClickListener { view ->
+                            dismiss()
+                        }
+                        tvMessageDialogRightBtn.visibility = View.GONE
+                    }
+                }
+                DialogType.ONE_BTN -> {
+                    binding.apply {
+                        tvMessageDialogMsg.text = message
+                        tvMessageDialogLeftBtn.text = leftBtnText
+                        tvMessageDialogLeftBtn.setOnClickListener { view ->
+                            leftBtnClickListener()
+                        }
+                        tvMessageDialogRightBtn.visibility = View.GONE
+                    }
+                }
+                DialogType.TWO_BTN -> {
+                    binding.apply {
+                        tvMessageDialogMsg.text = message
+                        tvMessageDialogLeftBtn.text = leftBtnText
+                        tvMessageDialogLeftBtn.setOnClickListener { view ->
+                            leftBtnClickListener()
+                        }
+                        tvMessageDialogRightBtn.text = rightBtnText
+                        tvMessageDialogRightBtn.setOnClickListener { view ->
+                            rightBtnClickListener()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private enum class DialogType {
+        /**
+         * 디폴트 타입
+         */
+        DEFAULT,
+
+        /**
+         * 메시지 + 디폴트 버튼 1개
+         */
+        ONLY_MSG,
+
+        /**
+         * 메시지 + 커스텀 버튼 1개
+         */
+        ONE_BTN,
+
+        /**
+         * 메시지 + 커스텀 버튼 2개
+         */
+        TWO_BTN
+    }
+
+    companion object {
+        const val TAG: String = "MessageDialog"
+    }
+
+}
