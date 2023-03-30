fe04959635e34ae4b4f801ebeb0a2927acc26777
ByeongSu Kim <kimbsu3000@gmail.com>
2023-03-30 11:37:37 +0900

    feat: StoneData 정의
    홈 화면과 인트로 화면에서 사용하는 StoneData를 정의함

---
+++ app/src/main/java/com/goormthon/mang9rme/common/data/StoneData.kt
@@ -0,0 +1,47 @@
+package com.goormthon.mang9rme.common.data
+
+/**
+ * 홈 화면에서 사용하는 돌 데이터 클래스
+ *
+ * @param imgUrl        돌 이미지 url
+ * @param stoneName     돌 이름
+ * @param time          기록한 시각
+ * @param address       기록한 위치의 주소
+ * @param lat           기록한 위치의 위도
+ * @param lng           기록한 위치의 경도
+ * @param level         돌 레벨
+ * @param rarity        돌 희귀도
+ * @param attack        공격력
+ * @param defense       방어력
+ * @param magicDefence  마법저항력
+ */
+data class StoneData(
+    val imgUrl: String,
+    val stoneName: String,
+    val time: String,
+    val address: String,
+    val lat: String,
+    val lng: String,
+    val level: String,
+    val rarity: String,
+    val attack: Int,
+    val defense: Int,
+    val magicDefence: Int
+) : java.io.Serializable {
+
+    override fun toString(): String {
+        return "${TAG}{" +
+                "imgUrl=$imgUrl, " +
+                "stoneName=$stoneName, " +
+                "time=$time, " +
+                "address=$address, " +
+                "lat=$lat, " +
+                "lng=$lng, " +
+                "level=$level, " +
+                "rarity=$rarity}"
+    }
+
+    companion object {
+        private const val TAG: String = "StoneData"
+    }
+}
